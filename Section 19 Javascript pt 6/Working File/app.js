e5db52c69669c0db954ba170077612a389559149
hm-qm <77652802+hm-qm@users.noreply.github.com>
2021-04-30 16:10:33 +1000

    All things loops

---
+++ Section 19 Javascript pt 6/Working File/app.js
@@ -0,0 +1,21 @@
+// Counting till 10 loop
+for (let i = 1; i<=10; i++){
+    console.log(i);
+}
+
+// Countdown loop
+for (let i = 100; i >= 0; i--){
+    console.log(i);
+}
+
+// Write a loop that prints:
+// 25
+// 20
+// 15
+// 10
+// 5
+// 0
+
+for(let i = 25; i >= 0; i -= 5){
+    console.log(i);
+}
