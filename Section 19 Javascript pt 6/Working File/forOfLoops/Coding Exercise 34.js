a536f9a21b83fd48bad7d91b7d1580c9b06c2999
hm-qm <77652802+hm-qm@users.noreply.github.com>
2021-05-05 16:21:59 +1000

    for of loops quiz

---
+++ Section 19 Javascript pt 6/Working File/forOfLoops/Coding Exercise 34.js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+// Print out the square of each value in numbers using for..of loop
+
+for (let num of numbers){
+    console.log(num*num)
+}
